b082863da2d0c94862ddcab1b29ec1cf1b4c2e77
邱俊 <i@qiujun.me>
2017-03-10 17:12:49 +0800

    修改数据库文件

--- shadowsocks/shadowsocks.sql
+++ shadowsocks/shadowsocks.sql
@@ -1,5 +1,5 @@
 CREATE TABLE `ss_user` (
-  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
   `port` int(11) NOT NULL,
   `password` varchar(32) NOT NULL,
   `flow_up` bigint(20) NOT NULL DEFAULT '0',
@@ -7,4 +7,13 @@ CREATE TABLE `ss_user` (
   `transfer_enable` bigint(20) NOT NULL,
   `is_locked` enum('Y','N') NOT NULL DEFAULT 'N',
   `active_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `ss_transfer` (
+  `transfer_id` int(11) NOT NULL,
+  `user_id` varchar(32) NOT NULL,
+  `node_id` int(11) NOT NULL,
+  `flow_up` bigint(20) NOT NULL DEFAULT '0',
+  `flow_down` bigint(20) NOT NULL DEFAULT '0',
+  `active_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
